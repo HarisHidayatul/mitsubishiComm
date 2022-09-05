944cc256170dd0bc6388bd1548f493539e4edaf7
HarisHidayatul <Hidayatulloh.haris@gmail.com>
2022-09-06 06:41:56 +0700

    Com Mitsubishi PLC

---
+++ mitsubishiComm/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace mitsubishiComm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void viewCom()
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Text = "\0";
+            try
+            {
+                foreach(string s in SerialPort.GetPortNames())
+                {
+                    comboBox1.Items.Add(s);
+                }
+                comboBox1.Text = comboBox1.Items[0].ToString();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            frameMitsubishi a = new frameMitsubishi();
+            //label2.Text = a.sum("5");
+            //label2.Text = String.Join("/", a.sendtoread(0,'d',123,2));
+            //label2.Text = String.Join("/", a.inttoHexinByte(0x1245,4));
+            //label2.Text = a.exponent(2,5).ToString();
+            //label2.Text = a.checkSum(new byte[] { 0x02, 0x30, 0x31, 0x30, 0x46, 0x36, 0x30, 0x32, 0x03, 0x37, 0x32 }).ToString();
+            label2.Text = a.hex1dtoInt('G').ToString();
+            label2.Text = a.hexToInt(new byte[] { 0x31, 0x31,0x32,0x33,0x34 },0,2).ToString();
+            viewCom();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.Open();
+                    if (serialPort1.IsOpen)
+                    {
+                        button1.Text = "DISCONNECT";
+                    }
+                }
+                else
+                {
+                    serialPort1.Close();
+                    button1.Text = "CONNECT";
+                }
+            }catch(Exception a)
+            {
+                MessageBox.Show(a.ToString());
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int j;
+            int i;
+            string hasil;
+            j = serialPort1.BytesToRead;
+            byte[] buffer = new byte[j];
+            for (i = 0; i < j; i++)
+            {
+                buffer[i] = (byte)serialPort1.ReadByte();
+                
+            }
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                label1.Text = label1.Text + BitConverter.ToString(buffer).Replace("-","");
+                label1.Text = label1.Text + " ";
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                //var dataByte = new byte[] { 0x02,0x30,0x31,0x30,0x46,0x36,0x30,0x32,0x03,0x37,0x32 };
+                //var dataByte = new byte[] {0x02,0x30,0x31,0x46,0x45,0x31,0x32,0x33,0x34,0x35, 0x36, 0x37, 0x38, 0x03, 0x39, 0x32 };
+                frameMitsubishi a = new frameMitsubishi();
+                List<byte> dataByte = new List<byte>();
+                dataByte = a.sendtoread(0, 'd', 0, 2);
+                serialPort1.Write(dataByte.ToArray(), 0, dataByte.Count);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+    }
+}
---
+++ mitsubishiComm/frameMitsubishi.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mitsubishiComm
+{
+    class frameMitsubishi
+    {
+        //this code refrence to manual refrence fx-232aw
+        //created by haris hidayatulloh - indonesia
+        const byte stx = 0x02;
+        const byte etx = 0x03;
+        public string a;
+        public List<byte> sendtoread(int command, char register, int address, int n)
+        {
+            List<byte> datasend = new List<byte>();
+            if(register == 'D'||register=='d')
+            {
+                datasend.AddRange(inttoHexinByte(command,1));
+                datasend.AddRange(inttoHexinByte(0x1000+(address*2),4));
+                datasend.AddRange(inttoHexinByte(n,2));
+            }
+            datasend.Add(etx);
+            datasend.AddRange(sumByte(datasend.ToArray()));
+            datasend.Insert(0, stx);
+            return datasend;
+        }
+        public bool checkSum(byte[] getbyte)
+        {
+            int length = getbyte.Length;
+            byte[] newByte = new byte[length];
+            byte[] checkSum = new byte[2];
+            if(length > 2)
+            {
+                for(int i = 0; i < (length - 2); i++)
+                {
+                    newByte[i] = getbyte[i];
+                }
+                checkSum = sumByte(newByte);
+                if (checkSum[0] == getbyte[length - 2])
+                {
+                    if(checkSum[1] == getbyte[length - 1])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<int> readRespon(byte[] getByte)
+        {
+            List<int> valRespoon = new List<int>();
+            int length = getByte.Length-3;
+            if (checkSum(getByte))
+            {
+                if (length % 2 == 0)
+                {
+                    for(int i = 0; i < length / 2; i++)
+                    {
+                        valRespoon.Add(hexToInt(getByte,i*2,2));
+                    }
+                    return valRespoon;
+                }
+                else
+                {
+                    return valRespoon;
+                }
+            }
+            else
+            {
+                return valRespoon;
+            }
+        }
+        public int hexToInt(byte[] getByte, int start, int length)
+        {
+            int sum = 0;
+            if (start+length <= getByte.Length)
+            {
+                length--;
+                for(int i = 0; i <= length; i++)
+                {
+                    sum = sum + hex1dtoInt((char)getByte[start+i])*exponent(16,length-i);
+                }
+                return sum;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public int hex1dtoInt(char gethex)
+        {
+            if ((gethex > 48) && (gethex < 58)) 
+            { 
+                return gethex - 48; 
+            }else if ((gethex > 64) && (gethex < 71))
+            {
+                return gethex - 55;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public char int1dtoHex(int getint)
+        {
+            getint = getint % 16;
+            if(getint < 10) { return (char)(getint + 48); }
+            else { return (char)(getint%10 + 65); }
+        }
+        public int exponent(int getInt,int n)
+        {
+            int sum = 1;
+            for(int i = 0; i < n; i++)
+            {
+                sum = sum*getInt;
+            }
+            return sum;
+        }
+        public List<byte> inttoHexinByte(int getInt,int length)
+        {
+            List<byte> hex = new List<byte>();
+            for (int i = length-1; i >= 0; i--)
+            {
+                //hex.Add((byte)((getInt / exponent(16, i))%16));
+                hex.Add((byte)int1dtoHex(getInt/exponent(16,i)));
+            }
+            return hex;
+        }
+        public byte[] sumByte(byte[] getByte)
+        {
+            int sum = 0;
+            for(int i=0;i<getByte.Length;i++)
+            {
+                sum = sum + getByte[i];
+            }
+            //convert byte to hex string
+            string s = BitConverter.ToString(new byte[] { (byte)(sum % 256) });
+            return new byte[] { (byte)s[0],(byte)s[1]};
+        }
+    }
+}
